f245135d9210256254d3ded438a5f7d8586f1875
Ellis Lunayo <ellislunayo@gmail.com>
2026-04-02 10:56:42 +0300

    added pip freeze on my requirements

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from extensions import db
 from routes.teams import teams_bp
 from routes.players import players_bp
@@ -7,10 +8,11 @@ from routes.coaches import coaches_bp
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dimba.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://dimba_user:dimba245%2A%2A123@localhost:5432/dimba"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+    Migrate(app, db)
 
     @app.route("/")
     def home():
